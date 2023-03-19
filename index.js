ca708671f01ecc34cb85ff73a3f65b1b0e793cc1
Asonwa Nnabeze <lilynnabeze@gmail.com>
2023-03-19 02:01:01 +0100

    fifth-assignment

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
-let form = document.getElementById('submit-form');
-let list = document.getElementById('incomplete-tasks');
+const form = document.querySelector('.submit-form');
 
-form.addEventListener('submit', addItem);
+const input = document.querySelector('.task')
 
-function addItem(event) {
+const ul = document.querySelector('.list')
+
+let lists = JSON.parse(localStorage.getItem('lists'));
+
+lists.forEach(task=>{
+   todoList(task)
+})
+
+form.addEventListener('submit', (event)=>{
+   
    event.preventDefault();
+   todoList();
+});
+
+function todoList(task){
+
+   let newTask = input.value;
+   if(task) {
+      newTask = task.name
+   }
+
+   let li = document.createElement('li')
+   if(task && task.checked) {
+      li.classList.add('todolist');
+   }
    
-   let newItem = document.getElementById('new-task').value;
-
-   let li = document.createElement('li');
-   
-   li.className = 'todolist';
-
-   li.appendChild(document.createTextNode(newItem));
-
-   let delButton = document.createElement('button');
-
-   delButton.className = 'delete-button';
-
-   delButton.appendChild(document.createTextNode('Delete'));
-
-   
-   li.appendChild(delButton);
    
 
-   list.appendChild(li);
+   li.innerText = newTask;
+   ul.appendChild(li);
+   input.value = '';
+   const checkBox = document.createElement('div');
+   checkBox.innerHTML = `<i class="fa-solid fa-square-check"></i>`
+   li.appendChild(checkBox);
+   const trashBtn = document.createElement('div');
+   trashBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`
+   li.appendChild(trashBtn);
+
+   checkBox.addEventListener('click', ()=>{
+      li.classList.toggle('todolist')
+      updateLocalStorage();
+      });
+
+      trashBtn.addEventListener('click', ()=>{
+         li.remove();
+         updateLocalStorage();
+      });
+
+      updateLocalStorage()
 }
 
-  
+function updateLocalStorage(){
+   const liElements = document.querySelectorAll('li');
+
+   lists = []
+   liElements.forEach(li=>{
+      lists.push({
+         name: li.innerText,
+         checked: li.classList.contains('todolist')
+      })
+   })
+
+   localStorage.setItem('lists', JSON.stringify(lists))
+}
+
+
